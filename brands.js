2402cc2aa106c7786ff3be081e2c4564754d93da
karolwasemann <kw.kwaq@gmail.com>
2022-06-09 10:15:08 +0200

    added brands data

---
+++ brands.js
@@ -0,0 +1,50 @@
+const brands = [
+  {
+    name: "kickstarter",
+    img: "https://stripe.com/img/v3/payments/overview/photos/kickstarter.jpg",
+    text: `<span class="text-gray-900">Kiskstarter</span> switched to
+                Stripe to accelerate their support for international creators.
+                Since partnering with Stripe to navigate international payment
+                methods and compliance, Kickstarter’s phenomenally successful
+                platform for creative projects is now available to creators in
+                18 countries.`
+  },
+  {
+    name: "slack",
+    img: "https://stripe.com/img/v3/payments/overview/photos/slack.jpg",
+    text: ` As <span class="text-gray-900">Slack</span> grows rapidly, using
+                Stripe helps them scale payments easily—supporting everything
+                from getting paid by users around the world to enabling ACH
+                payments for corporate customers.`
+  },
+  {
+    name: "glossier",
+    img: "https://stripe.com/img/v3/payments/overview/photos/glossier.jpg",
+    text: `With Stripe’s reliable and developer-friendly tools,<span
+                  class="text-gray-900"
+                  >Glossier’s</span
+                >engineering team is freed up to focus on product and customer
+                experiences.`
+  },
+  {
+    name: "charitywater",
+    img: "https://stripe.com/img/v3/payments/overview/photos/charity_water.jpg",
+    text: `<span class="js-texttext-gray-900">charity: water</span
+                >optimized their mobile and web donation flows with Stripe so
+                they can focus on what really matters: bringing clean water to
+                every person on the planet. Using Stripe, they also built a new
+                monthly giving program, which makes it even easier for
+                subscribers to donate throughout the&nbsp;year.`
+  },
+  {
+    name: "missguided",
+    img: "https://stripe.com/img/v3/payments/overview/photos/missguided.jpg",
+    text: ` <span class="text-gray-900">Missguided</span>is a UK-based
+                fashion retailer that has nearly doubled in size since last
+                year. They integrated Stripe to deliver seamless checkout across
+                mobile and web for customers in 100+ countries, all while
+                automatically combating&nbsp;fraud.`
+  }
+];
+
+export default brands;
